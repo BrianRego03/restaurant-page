a5296bbcf023c4c19f36077e233d1e5ce134f6b5
Brian rego <brian.rego03@gmail.com>
2024-07-29 14:05:52 +0530

    Create clearscreen functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
+// import crab from './pics/homepage/crabhome.png'
 
-console.log("Is this thing working?")
+let contentSection=document.querySelector("#content");
+
+function clearscreen(){
+    while (contentSection.hasChildNodes()) {
+        contentSection.removeChild(contentSection.firstChild);
+      }
+      console.log("Is this thing working?")
+}
+// clearscreen();
+
+
+// const mycrab=new Image();
+// mycrab.src=crab;
+// contentSection.appendChild(mycrab);
+
