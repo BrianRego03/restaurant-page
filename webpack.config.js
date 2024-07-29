620db63d7d96862516415b4e9b48cacff59389a8
Brian rego <brian.rego03@gmail.com>
2024-07-29 11:10:52 +0530

    Setup html webpack plugin to generate index html file

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const htmlwebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    mode:'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      watchFiles: ['src/index.html'],
+    },
+   
+    plugins: [
+      new htmlwebpackplugin({
+        template:'./src/index.html',
+        
+      })
+    ]
+    
+    
+  };
+  
+  
+  
